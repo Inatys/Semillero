87bfe83361c7dd6a6a76990f2795f1f19e1d9742
Inatys <54780848+Inatys@users.noreply.github.com>
2021-10-20 09:55:40 -0500

    Add files via upload

--- WorkOuts/6 Generación de facturas/design/script.js
+++ WorkOuts/6 Generación de facturas/design/script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
 	paginaInicio();
 });
 
-function paginaInicio() {	
+function paginaInicio() {
 	divFiltro.innerHTML = ` 
 					<div class="fechas">
 						<div   class="fecha">
@@ -179,7 +179,7 @@ function consultar() {
 										<td id="textoModelo"><h4 class="descripcion">${dato.modeloNegocio}<h4></td>
 										<td><h4 class="descripcion">${dato.descripcionOrdenFacturacion}<h4></td>
 										<td>
-										<select name="" id="dias" onChange="validarSelect()">
+										<select name="" id="dias" onChange="select(${dato.codigoOrdenDeFacturacion},${dato.valorTotalACobrar})">
 											<option value="0">Selecciona Dias</option>
 											<option value="1">5</option>
 											<option value="2">10</option>
@@ -197,53 +197,65 @@ function consultar() {
 									</tr>								
 			`;
 			});
-
 			trTabla = document.querySelectorAll("#tablaDato");
 			tdSeleccion = document.querySelectorAll("#tdSeleccion");
 			habilitarSeleccion = document.querySelectorAll("#dias");
+			habilitarSelecciones = Array.from(document.querySelectorAll("#dias"));
+			diasSelect = document.getElementById("dias");
 			filtrarFactura();
 		});
 	botonGenerar = document.getElementById("generar");
 	botonGenerar.disabled = true;
 }
+let diasSelect;
 let contenidoTabla;
 let trTabla;
 let li;
 let tdSeleccion;
 let habilitarSeleccion;
+let habilitarSelecciones;
 let botonGenerar;
 let generaFactura;
 let tbodyFactura = document.getElementById("tablaFactura");
-
+let arr = [];
 function validarSelect() {
 	for (let i = 0; i < habilitarSeleccion.length; i++) {
-		console.log(habilitarSeleccion[i]);
+		/* 	console.log(habilitarSeleccion[i]);
 		console.log(tdSeleccion[i]);
-		console.log(habilitarSeleccion[i].value);
+		console.log(habilitarSeleccion[i].value); */
 		if (habilitarSeleccion[i].value != "0") {
 			tdSeleccion[
 				i
 			].innerHTML = `<img class="iconosTabla" id="imagenSeleccion" src="./img/seleccionado.svg" alt="" />`;
-			/* tbodyFactura.innerHTML=`
-					<td>641</td>
-								<td>FE</td>
-								<td>SETT1150</td>
-								<td>COP $42.825,291,600</td>
-								<td>
-									<img class="iconosTabla" src="./img/icono adobe azul.svg" alt="" />
-									<img class="iconosTabla" src="./img/icono documento azul.svg" alt="" />
-								</td>
-					` */
-			console.log("selecciono");
 			botonGenerar.disabled = false;
 			botonGenerar.style.background = "#2267a0";
+
+			/* console.log("selecciono")
+			console.log(taba) */
 		} else if (habilitarSeleccion[i].value === "0") {
-			console.log("no selecciono");
+			/* console.log("no selecciono"); */
 			tdSeleccion[
 				i
 			].innerHTML = `<img class="iconosTabla" id="imagenSeleccion" src="./img/noSeleccionado.svg" alt="" />`;
 		}
 	}
+}
+function rellenar(dato1, dato2) {
+	/*diasSelect.addEventListener('change', (event) => {
+     console.log(event.target.value);
+}); */
+	console.log(diasSelect);
+	console.log(diasSelect.value); 
+	habilitarSelecciones.map((select)=>{
+		select.addEventListener('change',()=>{
+			console.log("este");
+		})
+	})
+}
+
+function select(dato1, dato2) {
+	rellenar(dato1, dato2);
+	validarSelect();
 }
 
 function generarFactura() {
@@ -255,6 +267,7 @@ function generarFactura() {
 
 let valorFechaInicial;
 let valorFechaFinal;
+
 function validarFechas() {
 	valorFechaInicial = fechaInicial.value;
 	valorFechaFinal = fechaFinal.value;
@@ -290,25 +303,29 @@ function filtrarFactura() {
 		textoModelo = contenidoModelo.textContent;
 		textoFechaInicial = contenidoFechaInicial.textContent;
 		textoFechaFinal = contenidoFechaFinal.textContent;
+		console.log("texto factura" + textoFactura.toUpperCase().indexOf(filterFactura));
+		console.log("texto cliente" + textoCliente.toUpperCase().indexOf(filterCliente));
+		console.log("texto modelo" + textoModelo.toUpperCase().indexOf(filterModelo));
+		console.log(
+			"texto fecha inicial" + textoFechaInicial.toUpperCase().indexOf(filterFechaInicial)
+		);
+		console.log("texto fecha final" + textoFechaFinal.indexOf(filterFechaFinal));
 
 		if (
-			((textoFactura.toUpperCase().indexOf(filterFactura) &&
-				textoFechaInicial.indexOf(filterFechaInicial) &&
-				textoFechaFinal.indexOf(filterFechaFinal) &&
-				textoCliente.toUpperCase().indexOf(filterCliente) &&
-				textoModelo.toUpperCase().indexOf(filterModelo)) ||
+			(textoFactura.toUpperCase().indexOf(filterFactura) ||
 				textoCliente.toUpperCase().indexOf(filterCliente) ||
-				textoFechaInicial.indexOf(filterFechaInicial) ||
-				textoFechaFinal.indexOf(filterFechaFinal) ||
-				textoFactura.toUpperCase().indexOf(filterFactura) ||
 				textoModelo.toUpperCase().indexOf(filterModelo)) > -1
 		) {
 			trTabla[i].style.display = "";
 			//trTabla[i].style.display = "none";
+			console.log(" se encontraron");
 		} else {
 			trTabla[i].style.display = "none";
 			console.log("los filtors no se encontraron");
 			//trTabla[i].style.display = "";
 		}
+
+		//textoFechaInicial.indexOf(filterFechaInicial) ||
+		//textoFechaFinal.indexOf(filterFechaFinal) ||
 	}
 }
