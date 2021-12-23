37fda44a179f79ec2460338bb4cff651f8c8edf7
Inatys <54780848+Inatys@users.noreply.github.com>
2021-12-22 19:13:44 -0500

    Add files via upload

--- WorkOuts/paginaMaquinaria/script3.js
+++ WorkOuts/paginaMaquinaria/script3.js
@@ -27,10 +27,11 @@ function ocultarSeccionTarifas() {
 }
 
 
+
+
 let seccionesEquipos = document.querySelector(".seccionesEquipos");
 function pintarDivsEquipos(mostrarDatosJson){
 mostrarDatosJson.forEach((dato)=>{
-console.log(dato.nombreEquipo)
 seccionesEquipos.innerHTML += `
             <div class="divEquipo">
                 <h6>${dato.nombreEquipo}</h6>
@@ -38,18 +39,17 @@ seccionesEquipos.innerHTML += `
 		    </div>                 
             `;
 })
+
 }
 
 let seccionesTarifas = document.querySelector(".seccionesTarifas");
  function pintarDivsTarifas(mostrarDatosJson){
     mostrarDatosJson.forEach((data)=>{	
-        console.log(data.tarifaPGR)
                 seccionesTarifas.innerHTML += `
                 <div class="divTarifas">
                     <span>$ ${data.tarifaPGR}</span>
                     <span>$ ${data.tarifaRI}</span>
-                </div>
-                     
+                </div>                     
                 `;
                /*  bodyCalendario.innerHTML += `
                 <tr id="filaTarifas"></tr>			
@@ -60,21 +60,78 @@ let seccionesTarifas = document.querySelector(".seccionesTarifas");
         })
  }
 
+ function pintarFilasEnBodyCalendario(mostrarDatosJson,nombreEquipo){
+     let filaTarifas
+    mostrarDatosJson.forEach(datos => {
+        bodyCalendario.innerHTML += `
+                <tr id="filaTarifas"></tr>			
+                `;
+                filaTarifas = document.querySelectorAll("#filaTarifas");
+                
+         if(datos.codigo!=-1){
+             let datosCalendarioCadena=datos.calendario             
+                let datosCalendarioTransformados=JSON.parse(datosCalendarioCadena)
+        console.log(datosCalendarioTransformados)   
+         }
+     });
+      console.log(filaTarifas)
+      mostrarDatosJson.forEach(datos => {
+     for (let i = 0; i < filaTarifas.length; i++) {       
+            filaTarifas[i].innerHTML += `
+                <td>
+                    <select id="selectDatoCalendario">
+                        <option  value="" disabled selected>Tarifas</option>
+                        <option>M</option>
+                        <option>I</option>
+                        <option>T1</option>
+                        <option>T2</option>
+                        <option>TP</option>
+                        <option>A</option>
+                    </select>
+                </td>				
+                `;
+        
+    } })
+ }
+
 function pintarDatosCalendario(datosCalendario) {
 	datosCalendario.forEach((element) => {
-		console.log(element.calendario)
-		/* cabeceraTabla.innerHTML += `
-				<th><p class="dias">${element.diaDesc.substring(0, 2)}</p><p>${element.fecha.substring(0, 2)}</p></th>
-			`; */
-	});
+		if (element.codigo === -1) {
+			calendario.forEach(element => {
+				console.log(element)
+			});
+				
+		/* 	cabeceraTabla.innerHTML += `
+				<th><p class="dias">${element.calendario[1].DescripcionDia}</p></th>
+		`;  */
+	};
+});
 }
-
-
 
 const pintarDatosJson=async()=>{
 	let informacionServicio=await traerDatosJson()
 	pintarDivsEquipos(informacionServicio);
 	pintarDatosCalendario(informacionServicio);
-    pintarDivsTarifas(informacionServicio)
+    pintarDivsTarifas(informacionServicio);
+    pintarFilasEnBodyCalendario(informacionServicio);
 }
 pintarDatosJson();	
+
+let inputFiltrarEquipo = document.getElementById("inputFiltrarEquipo");
+inputFiltrarEquipo.addEventListener("keyup", filtrar);
+function filtrar() {
+	var filter, seccionFiltro, divEntreSeccionFiltro, textosFiltrar, valorTextoFiltrado;
+	filter = inputFiltrarEquipo.value.toUpperCase();
+	seccionFiltro = document.querySelector(".seccionesEquipos");
+	divEntreSeccionFiltro = document.querySelectorAll(".divEquipo");
+	divTarifas = document.querySelectorAll(".divTarifas");
+	for (let i = 0; i < divEntreSeccionFiltro.length; i++) {
+		textosFiltrar = divEntreSeccionFiltro[i].querySelectorAll("h6")[0];
+		valorTextoFiltrado = textosFiltrar.textContent 
+		if (valorTextoFiltrado.toUpperCase().indexOf(filter) > -1) {
+			divEntreSeccionFiltro[i].style.display = "";
+		} else {
+			divEntreSeccionFiltro[i].style.display = "none";
+		}
+	}
+}
